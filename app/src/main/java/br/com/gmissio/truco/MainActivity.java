75e1c72924791b728585e49c9ac3a7d17ff1a58e
gabrielmissio <gabmissil@gmail.com>
2020-12-20 14:26:38 -0300

    Cpu jogando e jogo controlando jogadas e marcando ponto

--- app/src/main/java/br/com/gmissio/truco/MainActivity.java
+++ app/src/main/java/br/com/gmissio/truco/MainActivity.java
@@ -17,12 +17,18 @@ public class MainActivity extends AppCompatActivity {
     ImageView card1;
     ImageView card2;
     ImageView card3;
+    ImageView imgCard4;
+    ImageView imgCard5;
+    ImageView imgCard6;
     ImageView imgCard1Player;
     ImageView imgCard2Player;
     ImageView imgCard3Player;
     ImageView imgCard1Cpu;
     ImageView imgCard2Cpu;
     ImageView imgCard3Cpu;
+    TextView pontosTimeA;
+    TextView pontosTimeB;
+
     TextView textView;
     Jogo jogo = new Jogo(2);
 
@@ -43,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
         imgCard2Cpu = findViewById(R.id.imgCard2Cpu);
         imgCard3Cpu = findViewById(R.id.imgCard3Cpu);
 
+        imgCard4 = findViewById(R.id.imgCard4);
+        imgCard5 = findViewById(R.id.imgCard5);
+        imgCard6 = findViewById(R.id.imgCard6);
+
+        pontosTimeA = findViewById(R.id.pontosTimeA);
+        pontosTimeB = findViewById(R.id.pontosTimeB);
 
        // textView.append("mais essa");
         System.out.println("---------------------------------");
@@ -108,21 +120,25 @@ public class MainActivity extends AppCompatActivity {
         Jogador jogador = jogo.getJogadores().get(0);
         Carta cartaJogada = jogador.getCartas().get(indexCard);
         Carta cartaCpu = jogo.cpuPlayCard();
-//        switch (cardRodada){
-//            case 0:
-//                imgCard1Player.setVisibility(View.VISIBLE);
-//                imgCard1Player.setImageResource(cartaJogada.getIdImage());
-//                break;
-//            case 1:
-//                imgCard2Player.setVisibility(View.VISIBLE);
-//                imgCard2Player.setImageResource(cartaJogada.getIdImage());
-//                break;
-//            case 2:
-//                imgCard3Player.setVisibility(View.VISIBLE);
-//                imgCard2Player.setImageResource(cartaJogada.getIdImage());
-//                break;
-//
-//        }
+
+        switch (cardRodada){
+            case 0:
+                imgCard4.setVisibility(View.INVISIBLE);
+                imgCard1Cpu.setVisibility(View.VISIBLE);
+                imgCard1Cpu.setImageResource(cartaCpu.getIdImage());
+                break;
+            case 1:
+                imgCard5.setVisibility(View.INVISIBLE);
+                imgCard2Cpu.setVisibility(View.VISIBLE);
+                imgCard2Cpu.setImageResource(cartaCpu.getIdImage());
+                break;
+            case 2:
+                imgCard6.setVisibility(View.INVISIBLE);
+                imgCard3Cpu.setVisibility(View.VISIBLE);
+                imgCard3Cpu.setImageResource(cartaCpu.getIdImage());
+                break;
+
+        }
 
         switch (indexCard){
 
@@ -140,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 2:
                         imgCard3Player.setVisibility(View.VISIBLE);
-                        imgCard2Player.setImageResource(cartaJogada.getIdImage());
+                        imgCard3Player.setImageResource(cartaJogada.getIdImage());
                         break;
 
                 }
@@ -159,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 2:
                         imgCard3Player.setVisibility(View.VISIBLE);
-                        imgCard2Player.setImageResource(cartaJogada.getIdImage());
+                        imgCard3Player.setImageResource(cartaJogada.getIdImage());
                         break;
 
                 }
@@ -178,12 +194,18 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 2:
                         imgCard3Player.setVisibility(View.VISIBLE);
-                        imgCard2Player.setImageResource(cartaJogada.getIdImage());
+                        imgCard3Player.setImageResource(cartaJogada.getIdImage());
                         break;
 
                 }
                 break;
 
+        }
+        if(jogo.oneVsCpu(cartaJogada, cartaCpu)){
+            System.out.println("Pontooooo: " + jogo.getPontosTimeA() + " --- " + jogo.getPontosTimeB());
+            pontosTimeA.setText(String.valueOf(jogo.getPontosTimeA()));
+            pontosTimeB.setText(String.valueOf(jogo.getPontosTimeB()));
+            //pontosTimeB.setText(jogo.getPontosTimeB());
         }
 
     }
